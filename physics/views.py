4696804336373dea59d2fde0e4c2bf7d829c9635
user <sheldd49@gmail.com>
2024-05-20 20:29:53 +0300

    result verst

--- physics/views.py
+++ physics/views.py
@@ -1,14 +1,28 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader, Context
+from django.template import loader
+from .models import Task
 
 def homepage(request):
     template = loader.get_template('homepage.html')
     return HttpResponse(template.render())
 
 def quiz(request):
+    tasks = Task.objects.all().values()
     template = loader.get_template('quiz.html')
-    return HttpResponse(template.render())
-    
+    context = {
+        'tasks':tasks,
+    }
+    return HttpResponse(template.render(context,request))
+
+
+def result(request):
+    tasks = Task.objects.all().values()
+    template = loader.get_template('result.html')
+    context = {
+        'tasks':tasks,
+    }
+    return HttpResponse(template.render(context,request))
+  
 
 # Create your views here.
