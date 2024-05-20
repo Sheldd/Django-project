b801efec99c87baf5bd23419566137378244f888
user <sheldd49@gmail.com>
2024-05-20 16:47:38 +0300

    add admin (superuser)

--- physics/admin.py
+++ physics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id","section")
+
+admin.site.register(Task,TaskAdmin)
--- physics/models.py
+++ physics/models.py
@@ -6,3 +6,6 @@ class Task(models.Model):
     section = models.CharField(max_length = 50)
     task = models.TextField()
     answer = models.IntegerField()
+    
+    def __str__(self):
+        return(f"{self.id}{self.section}")
