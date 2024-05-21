3c4a02875f398ff8aa6f46bf25bb5024463d275e
user <sheldd49@gmail.com>
2024-05-21 04:20:10 +0300

    404 changes and bugfix=false

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-s9h*a6j(sp133u+3&qz4(^4+dve+o4=b0hedb7!q*-+d^%63!-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
